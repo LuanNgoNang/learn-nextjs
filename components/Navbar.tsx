6af69f007d32e77b03d90e82f67c0558d7cd96bb
LuanSmall <luan1408997@gmail.com>
2023-08-09 23:12:20 +0700

    Update Code CV

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link'
 export const Navbar = () => {
   return (
   <div>
-    <h1>Thanh Luan</h1>
+    <h1>Nguyễn Thành Luân</h1> 
+    <h4>Software Engineer</h4>
     <nav>
         <div className='logo'>
-        </div>
-        <Link href="/"><a>Home</a></Link>
-        <Link href="/about"><a>Page</a></Link>
-        <Link href="/posts"><a>About</a></Link>
+        </div> 
+        <Link href="/"><a>About Me</a></Link>
+        <Link href="/about"><a>Work Experience</a></Link>
+        <Link href="/posts"><a>Contact</a></Link>
     </nav>
 
   </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import { Footer } from '../components/Footer'
-import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -15,6 +11,7 @@ const Home: NextPage = () => {
       <Link href={"/ninjas "}>
       <p className={styles.btn}>Link About</p>
       </Link>
+
     </div>
   )
 }
